c44ddf6301cc03a54960201962e80cf114ca69c3
mcarbaugh <michael.s.carbaugh@gmail.com>
2017-04-04 20:10:12 -0500

    Added a second URL to the DatabaseFactory class for connecting to local/remote databases.

--- src/java/budgetApplication/dataAccess/DatabaseFactory.java
+++ src/java/budgetApplication/dataAccess/DatabaseFactory.java
@@ -6,14 +6,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseFactory implements AutoCloseable {
-    private static final String url = "jdbc:mysql://acadmysql.duc.auburn.edu/BudgetDB";
+    private static final String url_local = "jdbc:mysql://localhost:3306/BudgetDB";
+    private static final String url_remote = "jdbc:mysql://acadmysql.duc.auburn.edu/BudgetDB";
     private static final String dbUser = "comp6000";
     private static final String dbPassword = "BudgetDB";
     
     public static Connection getMySqlConnection() throws Exception {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            return DriverManager.getConnection(url, dbUser, dbPassword);
+            return DriverManager.getConnection(url_local, dbUser, dbPassword);
         }
         catch(ClassNotFoundException | SQLException ex) {
             throw ex;
