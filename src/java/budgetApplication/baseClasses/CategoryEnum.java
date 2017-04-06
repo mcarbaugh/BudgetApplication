8aa5ee8ad6aacfc9c7076ae524e9db33196b4496
mcarbaugh <michael.s.carbaugh@gmail.com>
2017-04-06 01:27:14 -0500

    Merge

--- src/java/budgetApplication/baseClasses/CategoryEnum.java
+++ src/java/budgetApplication/baseClasses/CategoryEnum.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package budgetApplication.baseClasses;
 
-/**
- *
- * @author Eclat
- */
 public enum CategoryEnum {
     NONE (""),
     GIVING ("Giving"),
@@ -25,6 +17,7 @@ public enum CategoryEnum {
         name = value;
     }
     
+    @Override
     public final String toString() {
         return this.name;
     }
