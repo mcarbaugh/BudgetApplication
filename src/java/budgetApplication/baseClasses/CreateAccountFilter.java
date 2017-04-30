591ee6866cd03c6d6ded340b260532e16b8e968e
mcarbaugh <michael.s.carbaugh@gmail.com>
2017-04-30 10:45:00 -0500

    Added a web filter that clears the account creation form if the user leaves the page.

---
+++ src/java/budgetApplication/baseClasses/CreateAccountFilter.java
@@ -0,0 +1,39 @@
+
+package budgetApplication.baseClasses;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter({"/CreateAccount"})
+public class CreateAccountFilter implements Filter {
+    
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        
+        res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        res.setHeader("Pragma", "no-cache");
+        res.setDateHeader("Expires", 0);
+        
+        chain.doFilter(request, response);
+    }
+    
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        
+    }
+
+    @Override
+    public void destroy() {
+        
+    }
+}
